d461e25bbd2cf793cc0cb1758f33ce9335c35a31
NikitaRusanovskii <nrusanovskii923@gmail.com>
2026-05-23 20:42:09 +0300

    bugfix: run_server.sh procedure

--- Scripts/run_server.sh
+++ Scripts/run_server.sh
@@ -2,9 +2,10 @@
 
 source .env
 
-# "$GOLANG_MIGRATE" -path "$MIGRATIONS_DIR" -database "$DATABASE_URL?sslmode=disable" down
-"$GOLANG_MIGRATE" -path "$MIGRATIONS_DIR" -database "$DATABASE_URL?sslmode=disable" up
 
 cd ../
 sudo docker compose up
+
+# "$GOLANG_MIGRATE" -path "$MIGRATIONS_DIR" -database "$DATABASE_URL?sslmode=disable" down
+"$GOLANG_MIGRATE" -path "$MIGRATIONS_DIR" -database "$DATABASE_URL?sslmode=disable" up
 
